e851e335d7b4c266764ba59240ea01d04e64eb54
MateuszZak0 <akmateusz4@gamil.com>
2022-06-08 16:35:37 +0200

    punkt 8

--- src/main/java/pl/lublin/wsei/java/cwiczenia/console/TestBinCopy.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/console/TestBinCopy.java
@@ -10,7 +10,7 @@ public class TestBinCopy {
 
         FileInputStream in = null;
         FileOutputStream out = null;
-
+ long startTime = System.nanoTime();
         try
         {
             in = new FileInputStream("sample_1920×1280.tiff");
@@ -38,5 +38,12 @@ public class TestBinCopy {
                 out.close();
             }
         }
+        long endTime = System.nanoTime();
+
+        long timeElapsed = endTime - startTime;
+
+        System.out.println("Czas wykonania w nanosekundach : " + timeElapsed);
+        System.out.println("Czas wykonania w milisekundach : " + timeElapsed/1000000);
+
     }
 }
